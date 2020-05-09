a15709c04bcc7b3eaaaceedd7abe365b137f07c1
Dan Canetti <info@danielcanetti.com>
2020-05-09 23:38:42 +0100

    Readding feed.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 // Won't compile
-//const pluginRss = require("@11ty/eleventy-plugin-rss");
-//module.exports = function(eleventyConfig) {
-//  eleventyConfig.addPlugin(pluginRss);
-//};
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+};
