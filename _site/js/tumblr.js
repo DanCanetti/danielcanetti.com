80fff45de6113e0c3bb9118e00ab4cb0ff5acfc9
Dan Canetti <info@danielcanetti.com>
2020-05-28 22:35:04 +0100

    Finish styling moodboard.

--- _site/js/tumblr.js
+++ _site/js/tumblr.js
@@ -9,14 +9,14 @@ $.ajax({
 for (var i in postings) {
        var p = postings[i];
   if(p.type == 'photo'){
-    text += '<div class="case-study-item">' + '<a href='+ p.post_url +' target="_blank">' + '<img src=' +  p.photos[0].original_size.url + '>' + '</a></div>';
+    text += '<div class="case-study-item moodboard">' + '<a href='+ p.post_url +' target="_blank">' + '<img src=' +  p.photos[0].original_size.url + '>' + '</a></div>';
   }
     if(p.type == 'video'){
-    text += '<div class="video-container">' + p.player[0].embed_code + '</div>' +'<p></p>' + p.caption;
+    text += '<div class="case-study-item moodboard">' + p.player[0].embed_code + '</div>';
   }
       if(p.type == 'text'){
-    text += p.body + p.caption;
-  }
+        text += '<div class="case-study-item moodboard">' + p.body + '</div>';
+      }
       }
       $('#tumblrfeed').append(text);
     }
