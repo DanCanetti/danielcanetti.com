d4b58a1d19f8b82773e5d6b31bc2ff5a5a256a11
Dan <dacanetti@gmail.com>
2020-11-17 19:51:29 +0000

    FOUC fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,10 +7,33 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'docs/dist'),
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'ctti.css',
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+            },
+          },
+        ],
+        /*
         use: [
           'style-loader',
           'css-loader',
@@ -23,6 +47,7 @@ module.exports = {
             },
           },
         ],
+        */
       },
       {
         test: /\.(png|svg|jpg|gif|ico)$/,
